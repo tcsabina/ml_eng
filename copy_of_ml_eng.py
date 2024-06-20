17aabebb6d3484838c448029cf0584224ecfbd4d
ESAAD\Tamas.Csabina <tamas.csabina@ext.esa.int>
2024-06-21 00:07:30 +0200

    DataPreprocessor class moved to separate source file

--- copy_of_ml_eng.py
+++ copy_of_ml_eng.py
@@ -30,84 +30,7 @@ from sklearn.metrics import (
 # Own classes
 import DataAcquisition
 import DataFrameLoader
-
-class DataPreprocessor:
-    def __init__(self, dataframes):
-        """
-        Initialize the DataPreprocessor with the dataframes to be preprocessed.
-
-        :param dataframes: A dictionary of pandas dataframes to be processed.
-        """
-        self.dataframes = dataframes
-
-    def preprocess(self):
-        """
-        Perform the preprocessing steps on the dataframes.
-
-        :return: A processed pandas dataframe.
-        """
-        # Merge dataframes
-        patient_df = self.dataframes['patients']
-        admission_df = self.dataframes['admissions']
-        icustays_df = self.dataframes['icustays']
-        drgcodes_df = self.dataframes['drgcodes']
-        services_df = self.dataframes['services']
-        callout_df = self.dataframes['callout']
-
-        patient_df = patient_df.merge(admission_df, on="subject_id", how="left", suffixes=('_patient', '_admission')) \
-                       .merge(callout_df, on=["subject_id", "hadm_id"], how="left", suffixes=('_admission', '_callout')) \
-                       .merge(icustays_df, on=["subject_id", "hadm_id"], how="left", suffixes=('_callout', '_icustays')) \
-                       .merge(drgcodes_df, on=["subject_id", "hadm_id"], how="left", suffixes=('_icustays', '_drgcodes')) \
-                       .merge(services_df, on=["subject_id", "hadm_id"], how="left", suffixes=('_drgcodes', '_services'))
-
-        # Drop empty and unnecessary columns
-        patient_df.dropna(how="all", axis="columns", inplace=True)
-        print(patient_df.columns.tolist())
-
-
-        columns_to_drop = [
-            "row_id_x",
-            "row_id_y",
-            "dod_hosp",
-            "dod_ssn",
-            "language",
-            "religion",
-            "marital_status",
-            "edregtime",
-            "edouttime",
-            "diagnosis",
-            "has_chartevents_data"
-            "submit_wardid",
-            "submit_careunit",
-            "curr_wardid",
-            "curr_careunit",
-            "callout_wardid",
-            "request_tele",
-            "request_resp",
-            "request_cdiff",
-            "request_mrsa"]
-        patient_df.drop(columns_to_drop, axis=1, inplace=True, errors='ignore')
-
-        # Convert columns to lowercase
-        patient_df = patient_df.apply(lambda x: x.astype(
-            str).str.lower() if x.dtype == "object" else x)
-
-        # Fill 'callout_service' and 'callout_outcome' with mode
-        for column in ["callout_service", "callout_outcome"]:
-            mode = patient_df[column].mode()[0]
-            patient_df[column] = patient_df[column].fillna(mode)
-
-        # Fill 'createtime' and 'outcometime' with 'admittime'
-        patient_df["createtime"] = patient_df["createtime"].fillna(
-            patient_df["admittime"])
-        patient_df["outcometime"] = patient_df["outcometime"].fillna(
-            patient_df["admittime"])
-
-        # Fill 'deathtime' with 'dod' for patients not having hospital death
-        patient_df["deathtime"] = patient_df["deathtime"].fillna(
-            patient_df["dod"])
-
-        return patient_df
+import DataPreprocessor
 
 
 class DataVisualizer:
@@ -378,7 +301,7 @@ loader = DataFrameLoader.DataFrameLoader(file_paths)
 dataframes = loader.load_dataframes()
 
 # Instantiate DataPreprocessor()
-preprocessor = DataPreprocessor(dataframes)
+preprocessor = DataPreprocessor.DataPreprocessor(dataframes)
 
 # Invoke the preprocess method to perform all the preprocessing steps
 patients_df = preprocessor.preprocess()
